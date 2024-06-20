9aeae3a808b9f113d4dd498f175dbd169b869f9d
«Julia <jul.art1302@gmail.com>
2024-06-20 14:05:37 +0300

    first commit

---
+++ src/main/java/com/example/usejpa/UsejpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.usejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsejpaApplication.class, args);
+	}
+
+}
